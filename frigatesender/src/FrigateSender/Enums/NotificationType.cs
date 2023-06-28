2b5866e95040830adb6e2b90a9298b835707e679
Arnaud Dartois <nonobis@gmail.com>
2023-06-28 20:35:39 +0200

    Add NotificationType Enum

---
+++ frigatesender/src/FrigateSender/Enums/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrigateSender.Enums
+{
+    public enum NotificationType
+    {
+        Ntfy,
+        Telegram
+    }
+}
